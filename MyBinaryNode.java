befc73e4c2ae2721a618c004682db7bd59064e4c
shraazp <shravyap99@gmail.com>
2021-09-06 20:08:16 +0530

    added:ability to create nodes

---
+++ MyBinaryNode.java
@@ -0,0 +1,12 @@
+package binarysearchtree;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+	public MyBinaryNode(K key)
+	{
+		this.key=key;
+		this.left=this.right=null;
+	}
+}
---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,14 @@
+package binarysearchtree;
+
+public class MyBinaryTreeTest {
+
+	public static void main(String[] args) {
+		MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		System.out.println("size of tree is "+myBinaryTree.getSize());
+
+	}
+
+}
